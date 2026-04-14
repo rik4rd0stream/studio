ccaff48349e88c28383e734b2c03911d5834b550
rik4rd0stream <rik4rd0stream@gmail.com>
2026-04-14 14:15:06 -0300

    3

--- android/app/src/main/java/com/rappi/commander/MainActivity.java
+++ android/app/src/main/java/com/rappi/commander/MainActivity.java
@@ -1,5 +1,25 @@
 package com.rappi.commander;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Isso limpa as flags que fazem o app "vazar" para trás da barra de status
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // Define a cor da barra de status (Laranja Rappi)
+        window.setStatusBarColor(android.graphics.Color.parseColor("#E65C1A"));
+
+        // Força o conteúdo a respeitar os limites do sistema
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+    }
+}
